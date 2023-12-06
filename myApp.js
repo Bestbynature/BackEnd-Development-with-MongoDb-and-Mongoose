56c87657844f26883983cf7daeeb8b3d0479a516
Dammy <bestbynature2010@gmail.com>
2023-12-06 06:27:41 +0100

    Chain query operator

--- myApp.js
+++ myApp.js
@@ -198,6 +198,17 @@ removeManyPeople((err, result) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, data) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    });
+
   done(null /*, data*/);
 };
 
